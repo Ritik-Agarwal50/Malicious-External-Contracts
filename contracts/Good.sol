6bfeddf5bf968e4e3ba9b64923f460bd04aaa662
Ritik-Agarwal50 <ritikagarwal570@gmail.com>
2023-02-05 00:45:32 +0530

    first

---
+++ contracts/Good.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Ientifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.9;
+import "./Helper.sol";
+
+contract Good {
+    Helper helper;
+
+    constructor(address _helper) payable {
+        helper = Helper(_helper);
+    }
+
+    function isUserEligible() public view returns (bool) {
+        return helper.isUserEligible(msg.sender);
+    }
+
+    function addUserToList() public {
+        helper.setUserEligible(msg.sender);
+    }
+}
